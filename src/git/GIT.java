ac8058284d11744f7c966afdfc0720bc9b854150
Nitro <Nitro@DANY>
2024-11-05 11:17:04 -0600

    Confirmando mi teoria

--- src/git/GIT.java
+++ src/git/GIT.java
@@ -26,6 +26,9 @@ public class GIT {
                  
                  
                  System.out.println("AAAAAA" );
+                 System.out.println("Probando mi teoria" );
+                 
+                 
 
                 
     }
