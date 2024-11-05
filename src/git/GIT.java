a92c6b79802f99bb1572cdff079585ae31c9e6fa
Nitro <Nitro@DANY>
2024-11-05 02:57:30 -0600

    Puse otro comentario

--- src/git/GIT.java
+++ src/git/GIT.java
@@ -15,6 +15,8 @@ public class GIT {
      */
     public static void main(String[] args) {
                System.out.println("hola mundo" );
+               
+               System.out.println("Probando" );
 
     }
     
